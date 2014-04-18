11280d5cfc87e6f1b37c1074d854577b5d7333e7
Stevens.Wang <easykooc@gmail.com>
2014-04-18 14:07:58 +0800

    formatting for datatables

--- src/common/tsl.go
+++ src/common/tsl.go
@@ -16,12 +16,12 @@ func Translate(lang string, format string) string {
 	if lang == "" || !strings.Contains(lang, "zh") {
 		lang = "en"
 	}
-	return Tsl.MustValue(lang, format)
+	return Tsl.MustValue(lang, format, format)
 }
 
 func Translatef(lang string, format string, args ...interface{}) string {
 	if lang == "" || !strings.Contains(lang, "zh") {
 		lang = "en"
 	}
-	return fmt.Sprintf(Tsl.MustValue(lang, format), args)
+	return fmt.Sprintf(Tsl.MustValue(lang, format, format), args)
 }
